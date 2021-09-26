5bd1512f1d90988721eb546574de2be4f53c5a51
Syed A G Kazmi <kazmisag@gmail.com>
2021-09-26 19:34:03 +0200

    Trying to push ex1 and ex2

---
+++ ex2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+    
+    printf("\nI am %d old.", age);
+    printf("\nI am %d inches high", height);
+    
+    return 0;
+}
