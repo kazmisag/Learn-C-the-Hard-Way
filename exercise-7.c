8e04e99729b0f2419170c176fc3abcd341698e4d
Syed A G Kazmi <kazmisag@gmail.com>
2021-10-05 18:18:08 +0200

    exercise-7.c Used # and ## operators

--- exercise-7.c
+++ exercise-7.c
@@ -5,10 +5,17 @@ Make a program to print your full name to the terminal.
 Your full name shall be defined as a macro in the 
 command line when you compile your program.*/
 
-#define PRINT(name) "Syed Abbas Ghazanfar Kazmi"
+#define NAME_f "Syed"
+#define NAME_l "Kazmi"
+#define print_msg(n) puts(NAME_##n)
+
+#define PRINT(name) "SYED A G KAZMI"
 
 int main (void)
 {
     printf("%s\n", PRINT(name));
+    print_msg(f);
+    print_msg(l);
+   
     return 0;
 }
